75cd1cd573691a6915c4e06271d8079c4f11a0f7
milenayesayan <144199288+milenayesayan@users.noreply.github.com>
2024-04-10 03:25:08 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,296 +1,241 @@
-#ifndef SIMPLE_CITY_CITY_H
-#define SIMPLE_CITY_CITY_H
-
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <vector>
-#include <list>
-#include <cstring>
-#include <string>
 #include <iostream>
-#include <climits>
-#include <algorithm> 
-
-const int INF = 10000000;
+#include <vector>
+#include "City.h"
 
 using namespace std;
 
-class City {
-private:
+void test1();
 
-unordered_map<string, list<pair<string, int>>> adjacency_list;
-unordered_map<string, int> station_indices;
-vector<vector<int>> dist; 
-unordered_map<int, pair<string, string>> buses;
-unordered_set<string> crossroads;
+void test2();
 
-public:
+void test3();
 
-  unordered_map<string, list<pair<string, int>>> get_adjacency_list(){
-    return this->adjacency_list;
-  }
 
-  unordered_map<string, int> get_station_indices(){
-    return this->station_indices;
-  }
 
-  vector<vector<int>> get_dist_matrix(){
-    return this->dist;
-  }
+int main() {
 
-  unordered_map<int, pair<string, string>> get_buses(){
-    return this->buses;
-  }
+  test1();
 
-  unordered_set<string> get_crossroads(){
-    return this->crossroads;
-  }
+  test2();
 
-  void int_checker(int &input) {
-    while (!(cin >> input)) {
-        cout << "Invalid input. Please enter an integer: ";
-        cin.clear();  // Clear error flags
-        cin.ignore(INF, '\n');  // Discard invalid input
-    }
-  }
+  test3();
 
-  bool add_edge(const string src, const string dest, int duration) {
-    if(crossroads.find(src) != crossroads.end() && crossroads.find(dest) != crossroads.end()){
-        if(dist[station_indices[src]][station_indices[dest]] > duration)
-        adjacency_list[src].push_back({dest, duration});
-        adjacency_list[dest].push_back({src, duration}); 
-    }
-    else {
-        cout << "Invalid crossroad(s)." << endl;
-        return false;
-    }
-    return true;
-  }
+  City city;
+  int N = 0, M = 0;
+  cout << "Enter the number of stations (vertices): ";
+  city.int_checker(N);
+  cout << "Enter the stations: " << endl;
 
-  void print_adjacency_list() {
-    for (const auto& pair : adjacency_list) {
-        cout << "From Station " << pair.first << ":" << endl;
-        for (const auto& edge : pair.second) {
-            cout << "  To Station " << edge.first << " (Duration: " << edge.second << " minutes)" << endl;
-       }
-      cout << endl;
-    }
-  }
-
-  void initialize(int N) {
-    dist.assign(N, vector<int>(N, INF)); 
-    for (int i = 0; i < N; ++i) {
-        dist[i][i] = 0; 
-    }
-  }
-
-  void floyd_warshall(int n) {
-
-    for (int k = 0; k < n; ++k) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
-            }
-        }
-    }
-  }
-
-  void print_adjacency_list_matrix() {
-    cout << "\t";
-    for (const auto& pair : adjacency_list) {
-        cout << pair.first << "\t";
-    }
-    cout << endl;
-
-    for (const auto& pair : adjacency_list) {
-        cout << pair.first << "\t"; 
-        for (const auto& pair2 : adjacency_list) {
-            bool connected = false;
-            for (const auto& edge : pair.second) {
-                if (edge.first == pair2.first) {
-                    cout << edge.second << "\t";
-                    connected = true;
-                    break;
-                }
-            }
-            if (!connected) {
-                cout << "0\t"; 
-            }
-        }
-      cout << endl;
-    }
-  }
-
-  int min_time_required(const string& src, const string& dest) {
-    int u = station_indices[src];
-    int v = station_indices[dest];
-    return dist[u][v];
-  }
-
-  void add_bus(const string& A, const string& B) {
-    if(crossroads.find(A) == crossroads.end() || crossroads.find(B) == crossroads.end()) {
-        cout << "invalid stations" << endl;
-        return;
-    }
-    int min_time =  min_time_required(A, B);
-    if(min_time == INF) {
-        cout << "No connection" << endl;
-        return ;
-    }
-    buses[buses.size()] = {A, B};
-    cout << "Bus added with ID: " << buses.size() - 1 << ". Minimum time required for the bus to reach from " << A << " to " << B << ": " << min_time << " minutes" << endl;
-  }
-
-  int get_station_index(const string& station) {
-    if (station_indices.find(station) == station_indices.end()) {
-        int index = station_indices.size();
-        station_indices[station] = index;
-    }
-    return station_indices[station];
-  }
-
-  void print_buses() {
-    cout << "Available Buses:" << endl;
-    for (const auto& bus : buses) {
-        cout << "Bus ID: " << bus.first << ", Route: " << bus.second.first << " to " << bus.second.second << endl;
-    }
-  }
-
-  void construct_street(const string& A, const string& B, int duration) {
-    if(crossroads.find(A) != crossroads.end() && crossroads.find(B) != crossroads.end()) {
-        if(dist[station_indices[A]][station_indices[B]] >= duration){
-            int src = get_station_index(A);
-            int dest = get_station_index(B);
-            dist[src][dest] = duration;
-            dist[dest][src] = duration;
-          //change duration in the list
-            for(auto& edge : adjacency_list[A]){
-                if(edge.first == B){
-                    edge.second = duration;
-                    break;
-                }
-            }
-            for(auto& edge : adjacency_list[B]){
-                if(edge.first == A){
-                    edge.second = duration;
-                    break;
-                }
-            }
-        }
-    }
-    else {
-        cout << "Invalid crossroad(s)" << endl;
-        return;
-    }
-    cout << "Street constructed between " << A << " and " << B << " with duration " << duration << " minutes." << endl;
-    cout << "Minimum durations for all buses after constructing street:" << endl;
-    floyd_warshall_update(station_indices.size(), A, B, duration);
-
-    for (auto& bus : buses) {
-        auto start = bus.second.first;
-        auto destination = bus.second.second;
-        int busID = bus.first;
-        int new_duration = min_time_required(start, destination);
-        cout << "Bus ID: " << busID << ", Updated Duration: " << new_duration << " minutes" << endl;
-    }
-  }
-
-  void floyd_warshall_update(int num_vertices, const string& A, const string& B, int duration) {
-    int u = station_indices[A];
-    int v = station_indices[B];
-    dist[u][v] = duration;
-    dist[v][u] = duration;
-
-    for (int i = 0; i < num_vertices; ++i) {
-        if (dist[u][i] != INF && dist[i][v] != INF) {
-            dist[u][i] = min(dist[u][i], duration + dist[v][i]);
-            dist[i][u] = dist[u][i]; 
-        }
-        if (dist[v][i] != INF && dist[i][u] != INF) {
-            dist[v][i] = min(dist[v][i], duration + dist[u][i]);
-            dist[i][v] = dist[v][i]; 
-        }
-    }
+  for(int i = 0; i < N; i++){
+     string station;
+     cin >> station;
+     if(city.get_crossroads().find(station) != city.get_crossroads().end()){
+       cout << "Station already exists. Enter again: " << endl;
+       i--;
+     }
+     else {
+       city.get_crossroads().insert(station);
+     }
   }
   
-  void details(int bus_ID) {
-    if (buses.find(bus_ID) != buses.end()) {
-        auto start = buses[bus_ID].first;
-        auto destination = buses[bus_ID].second;
-        vector<vector<string>> shortest_paths = all_shortest_paths(start, destination);
-
-        if (shortest_paths.empty()) {
-            cout << "No shortest path found between " << start << " and " << destination << endl;
-        } 
-        else {
-            cout << "Shortest path(s) between " << start << " and " << destination << ":" << endl;
-            for (const auto& path : shortest_paths) {
-                for (const auto& node : path) {
-                    cout << node << " ";
-                }
-                cout << endl;
-            }
-        }
-    } 
-    else {
-        cout << "Bus ID not found!" << endl;
-    }
-  }
-
-  vector<vector<string>> all_shortest_paths(const string& start, const string& end) {
-    vector<vector<string>> shortest_paths;
-    int shortest_distance = INF; 
-    queue<pair<vector<string>, int>> q;
-    vector<string> initial_path;
-    initial_path.push_back(start);
-    q.push({initial_path, 0});
-
-    while (!q.empty()) {
-        auto current = q.front();
-        q.pop();
-        string last = current.first.back();
-        int current_distance = current.second;
-
-        if (last == end) {
-            if (current_distance < shortest_distance) {
-                shortest_distance = current_distance;
-                shortest_paths.clear();
-                shortest_paths.push_back(current.first);
-            } 
-            else if (current_distance == shortest_distance) {
-                  shortest_paths.push_back(current.first);
-            }
-        } 
-        else {
-            for (const auto& neighbor : adjacency_list[last]) {
-                if (find(current.first.begin(), current.first.end(), neighbor.first) == current.first.end()) {
-                    vector<string> new_path(current.first);
-                    new_path.push_back(neighbor.first);
-                    q.push({new_path, current_distance + neighbor.second});
-                }
-            }
-        }
-    }
-    return shortest_paths;
-  }
-
-  void fill_dist_matrix(){
-    for (const auto& pair : adjacency_list){   
-        int u = station_indices[pair.first];
-        for (const auto& edge : pair.second) {
-            int v = station_indices[edge.first];
-            dist[u][v] = edge.second;
-            dist[v][u] = edge.second; 
-        }
-    }
-  }
-
-  void fill_station_indices(){
-    for (const auto& pair : adjacency_list){
-        get_station_index(pair.first);
-    }
+  cout << "Enter the number of edges: ";
+  city.int_checker(M);
+  cout << "Enter the edges in format (u v k), u and v are stations,k is duration: " << endl;
+  
+  for (int i = 0; i < M; ++i) {
+      string u, v;
+      int k;
+      cin >> u >> v >> k;
+      bool edge_added = city.add_edge(u, v, k);
+      if(!edge_added) {
+        i--;
+      }
   }
   
-};
-#endif //SIMPLE_CITY_CITY_H
+  city.initialize(N);
+  city.floyd_warshall(N);
+  city.fill_dist_matrix();
+  city.fill_station_indices();
+
+  while (true) {
+      cout << "Operations:" << endl;
+      cout << "1. Add Bus" << endl;
+      cout << "2. Construct Street" << endl;
+      cout << "3. Details" << endl;
+      cout << "4. Quit" << endl;
+
+      int choice;
+      cout << "Enter your choice: ";
+      cin >> choice;
+
+      if (choice == 1) {
+          string A, B;
+          cout << "Enter source and destination for the bus route (Ex. A B): ";
+          cin >> A >> B;
+            city.add_bus(A, B);
+      } 
+      else if(choice == 2){
+          string A, B;
+          int dist;
+          cout << "Enter source and destination for the street (Ex. A B 3): ";
+          cin >> A >> B >> dist;
+          city.construct_street(A, B, dist);
+      } 
+      else if(choice == 3){
+          int busID;
+          cout << "Enter Bus ID: ";
+          cin >> busID;
+            city.details(busID);
+      } 
+      else if(choice == 4){
+          break;
+      } 
+      else {
+          cout << "Invalid choice. Please try again." << endl;
+      }
+  }
+
+  return 0;
+}
+
+void test1(){
+  cout << "____________________START OF TEST1__________________" << endl;
+
+  City city1;
+
+  cout << "Adding a stations: A, B, C" << endl;
+  city1.get_crossroads().insert("A");
+  city1.get_crossroads().insert("B");
+  city1.get_crossroads().insert("C");
+  
+  int N = city1.get_crossroads().size();
+
+  cout << "Adding an edge: (A, B, 3)" << endl;
+  city1.add_edge("A", "B", 3);
+  cout << "Adding an edge: (A, C, 2)" << endl;
+  city1.add_edge("A", "C", 2);
+  cout << "Adding an edge: (B, C, 1)" << endl;
+  city1.add_edge("B", "C", 1);
+  cout << "Adding an edge: (Q, W, 4)" << endl;
+  city1.add_edge("Q", "W", 4);
+  cout << "Adding an edge: (A, a, 12)" << endl;
+  city1.add_edge("A", "a", 12);
+  cout << endl;
+
+  city1.initialize(N);
+  city1.floyd_warshall(N);
+  city1.fill_dist_matrix();
+  city1.fill_station_indices();
+
+  cout << "Adding a bus route: (A, B)" << endl;
+  city1.add_bus("A", "B");
+  cout << "Adding a bus route: (A, C)" << endl;
+  city1.add_bus("A", "C");
+  cout << "Adding a bus route: (B, P)" << endl;
+  city1.add_bus("B", "P");
+  cout << endl;
+
+  cout << "1rt bus details" << endl;
+  city1.details(1);
+  cout << "0s bus details" << endl;
+  city1.details(0);
+  cout << "2rd bus details" << endl;
+  city1.details(2);
+  cout << endl;
+
+  cout << "Constructing a street: (A, C, 1)" << endl;
+  city1.construct_street("A", "C", 1);
+  cout << endl;
+
+  cout << "____________________END OF TEST1__________________" << endl;
+  cout << endl;
+
+}
+
+void test2(){
+  cout << "____________________START OF TEST2__________________" << endl;
+
+  City city2;
+
+  cout << "Adding a stations: e r t" << endl;
+
+  city2.get_crossroads().insert("e");
+  city2.get_crossroads().insert("r");
+  city2.get_crossroads().insert("t");
+
+  int N = city2.get_crossroads().size();
+
+  cout << "Adding an edge: (e, r, 27)" << endl;
+  city2.add_edge("e", "r", 3);
+  cout << "Adding an edge: (e, t, 20)" << endl;
+  city2.add_edge("e", "t", 2);
+  cout << "Adding an edge: (r, t, 17)" << endl;
+
+  city2.initialize(N);
+  city2.floyd_warshall(N);
+  city2.fill_dist_matrix();
+  city2.fill_station_indices();
+
+  cout << "Adding a bus route: (e, t)" << endl;
+  city2.add_bus("e", "t");
+  cout << "Adding a bus route: (e, r)" << endl;
+  city2.add_bus("e", "r");
+  cout << endl;
+
+
+  cout << "Constructing a street: (r, t, 100)" << endl;
+  city2.construct_street("r", "t", 100);
+  cout << "Constructing a street: (e, r, 15)" << endl;
+  city2.construct_street("e", "r", 15);
+  cout << "Constructing a street: (e, f, 12)" << endl;
+  city2.construct_street("e", "f", 12);
+  cout << endl;
+
+  cout << "____________________END OF TEST2__________________" << endl;
+  cout << endl;
+  
+}
+
+
+void test3(){
+  cout << "____________________START OF TEST3__________________" << endl;
+
+  City city3;
+
+  cout << "Adding a stations: b1 b2 b3 b4 b5" << endl;
+
+  city3.get_crossroads().insert("b1");
+  city3.get_crossroads().insert("b2");
+  city3.get_crossroads().insert("b3");
+  city3.get_crossroads().insert("b4");
+  city3.get_crossroads().insert("b5");
+
+  int N = city3.get_crossroads().size();
+
+  cout << "Adding an edge: (b1, b5, 207)" << endl;
+  city3.add_edge("b1", "b5", 207);
+  cout << "Adding an edge: (b2, b5, 455)" << endl;
+  city3.add_edge("b2", "b5", 455);
+  cout << "Adding an edge: (b2, b3, 187)" << endl;
+  city3.add_edge("b2", "b3", 187);
+
+  city3.initialize(N);
+  city3.floyd_warshall(N);
+  city3.fill_dist_matrix();
+  city3.fill_station_indices();
+
+  cout << "Constructing a street: (b1, b4, 100)" << endl;
+  city3.construct_street("b1", "b4", 100);
+  cout << "Constructing a street: (b2, b5, 150)" << endl;
+  city3.construct_street("b2", "b5", 150);
+  cout << "Constructing a street: (b4, b5, 120)" << endl;
+  city3.construct_street("b4", "b5", 120);
+  cout << endl;
+
+  cout << "____________________END OF TEST3__________________" << endl;
+  cout << endl;
+
+}
+
+
+
